f12a9f7cd55c136d08bac5fab6498e92ad9e9930
Ignacio Barbano <48802984+ignaciobarbanodesigner@users.noreply.github.com>
2023-04-05 10:27:23 -0300

    task-4 fix whata i broke on task-3

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
         {ejercicio === 1 &&<EjercicioDos/> }
         {ejercicio === 2 &&<EjercicioR52/>}
         <div>
-          <button onClick={()=>setEjercicio)}>Ejercicio 1</button>
+          <button onClick={()=>setEjercicio(0)}>Ejercicio 1</button>
           <button onClick={()=>setEjercicio(1)}>Ejercicio 2</button>
           <button onClick={()=>setEjercicio(2)}>Ejercicio 3</button>
         </div>
