a00b28c4df02552a914f8d50803ec0e8b8ad8558
Ignacio Barbano <48802984+ignaciobarbanodesigner@users.noreply.github.com>
2023-04-05 10:18:39 -0300

    task-2 make switch buttons for exercises

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,24 @@ import EjercicioDos from "./paginas/ejercicio-dos/EjercicioDos";
 import EjercicioR52 from "./paginas/ejercicio-r52/EjercicioR52";
 
 function App() {
-  const [viendoEjercicioUno, setViendoEjercicioUno] = useState(true);
+  const [ejercicio, setEjercicio] = useState(0);
 
   const cambiarColor = () => {
     setColor('blue');
   };
 
-  const onClick = () => {
-    setViendoEjercicioUno(!viendoEjercicioUno);
-  };
 
   return (
     <div className="App">
       <main>
-      <EjercicioR52/>
+        {ejercicio === 0 && <EjercicioUno/>}
+        {ejercicio === 1 &&<EjercicioDos/> }
+        {ejercicio === 2 &&<EjercicioR52/>}
+        <div>
+          <button onClick={()=>setEjercicio(0)}>Ejercicio 1</button>
+          <button onClick={()=>setEjercicio(1)}>Ejercicio 2</button>
+          <button onClick={()=>setEjercicio(2)}>Ejercicio 3</button>
+        </div>
       </main>
     </div>
   );
